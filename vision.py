38dc3037693e6777a3a477bf4e5c27542527f56b
rajesh <120458029+Alexanderwar@users.noreply.github.com>
2023-04-20 09:13:16 +0000

    comiting

--- vision.py
+++ vision.py
@@ -5,13 +5,39 @@ client = MongoClient('mongodb://localhost:27017/')
 db = client['mydatabase']
 collection = db['mycollection']
 
+# pipeline = [
+#     {
+#         # adding the match
+#         '$group': {
+#             '_id': {
+#                 '$dateToString': {
+#                     # 'format': '%Y-%m-%d %z',
+#                     'format': '%Y-%m',
+#                     'date': {
+#                         '$toDate': {
+#                             '$multiply': ['$epoch', 1000]
+#                         }
+#                     }
+#                 }
+#             },
+#             'count': {'$sum': 1},
+#             'documents': {'$push': '$$ROOT'}
+#         }
+#     }
+# ]
+
+queries = [{"country":"India"},{"pestle": "Industries"}]
+
 pipeline = [
+    # Filter documents that match any of the queries
+    # {'$match': {"country":"India"}},
+    {'$match': {"$and": queries}},
+    # Group documents by year and month
     {
         '$group': {
             '_id': {
                 '$dateToString': {
-                    # 'format': '%Y-%m-%d %z',
-                    'format':'%Y-%m',
+                    'format': '%Y-%m',
                     'date': {
                         '$toDate': {
                             '$multiply': ['$epoch', 1000]
@@ -27,9 +53,10 @@ pipeline = [
 
 results = collection.aggregate(pipeline)
 
+# count = collection.count_documents({"country": "India"})
+# print(count)
+
 for result in results:
-    print(result['_id'], result['count'])
-    # print(result['_id'], result['count'],"\n\n\n\n", result['documents'])
-    # for i in result['_id']:
-    #     print(i)
+    # print(result['_id'], result['count'])
+    print(result)
     break
